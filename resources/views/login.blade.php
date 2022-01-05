344fb3b91eade6c1fc7d81c5ab15ed45ef94f319
siemenvdv <siemen.vandervoort@student.uhasselt.be>
2022-01-05 18:49:58 +0100

    sso

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,13 +2,7 @@
 @section('content')
 <div class="main-background">
     <div class="main-title">
-        <a href="#" class="login-btn"> Sign in with Google </a>
-    </div>
-    <div class="main-title">
-        <a href="#" class="login-btn"> Sign in with Facebook </a>
-    </div>
-    <div class="main-title">
-        <a href="/sign-in/github" class="login-btn"> Sign in with Github </a>
+        <a href="/googleLogin" class="login-btn"> Sign in with Google </a>
     </div>
 </div>
 
