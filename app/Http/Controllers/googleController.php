89fc70d7d90ecb86c4a6de719c0697d5ddb8735d
lowie <defermelowie@gmail.com>
2022-01-09 11:49:21 +0100

    Move userdb to mariadb container

--- app/Http/Controllers/googleController.php
+++ app/Http/Controllers/googleController.php
@@ -10,28 +10,31 @@ use Auth;
 use Hash;
 use Str;
 
-class googleController extends controller{
-    
-    public function loginWithGoogle(){
+class googleController extends controller
+{
+
+    public function loginWithGoogle()
+    {
         return Socialite::driver('google')->redirect();
     }
-    /*
+
     //Gebruik deze functie als je jezelf wilt toevoegen aan de database
-    public function redirectFromGoogle(){
+    public function redirectFromGoogle()
+    {
         try {
             $user = Socialite::driver('google')->user();
 
             $is_user = User::where('email', $user->getEmail())->first();
-            if(!$is_user){
+            if (!$is_user) {
 
                 $saveUser = User::updateOrCreate([
                     'google_id' => $user->getId(),
-                ],[
+                ], [
                     'name' => $user->getName(),
                     'email' => $user->getEmail(),
-                    'password' => Hash::make($user->getName().'@'.$user->getId())
+                    'password' => Hash::make($user->getName() . '@' . $user->getId())
                 ]);
-            }else{
+            } else {
                 $saveUser = User::where('email',  $user->getEmail())->update([
                     'google_id' => $user->getId(),
                 ]);
@@ -41,37 +44,39 @@ class googleController extends controller{
             Auth::loginUsingId($saveUser->id);
 
             return redirect('/start');
+        } catch (\Throuwable $th) {
+            throw $th;
+        }
+    }
 
+    /*
+    //Gebruik deze functie als je alleen al eerder ingelogde gebruikers wilt laten inloggen
+    public function redirectFromGoogle()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+
+            $is_user = User::where('email', $user->getEmail())->first();
+            if (!$is_user) {
+                return redirect('/start');
+            } else {
+                $saveUser = User::where('email',  $user->getEmail())->update([
+                    'google_id' => $user->getId(),
+                ]);
+                $saveUser = User::where('email', $user->getEmail())->first();
+            }
+
+            Auth::loginUsingId($saveUser->id);
+
+            return redirect('/start');
         } catch (\Throuwable $th) {
             throw $th;
         }
     }
     */
-    
-    //Gebruik deze functie als je alleen al eerder ingelogde gebruikers wilt laten inloggen
-    public function redirectFromGoogle(){
-        try {
-            $user = Socialite::driver('google')->user();
 
-            $is_user = User::where('email', $user->getEmail())->first();
-            if(!$is_user){
-                return redirect('/start');                
-            }else{
-                $saveUser = User::where('email',  $user->getEmail())->update([
-                    'google_id' => $user->getId(),
-                ]);
-                $saveUser = User::where('email', $user->getEmail())->first();
-            }
-
-            Auth::loginUsingId($saveUser->id);
-
-            return redirect('/start');
-        } catch (\Throuwable $th) {
-            throw $th;
-        }
-    }
-
-    public function logOut(){
+    public function logOut()
+    {
         Auth::logout();
         return redirect('/start');
     }
